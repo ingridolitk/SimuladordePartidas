c0a8a18a4ae797fbf6368dd1b674d1b618db537b
Ingrid da Silva Oliveira <ingrid.oliveiratk@gmail.com>
2022-06-15 20:34:22 -0300

    layout em andamento

---
+++ app/src/main/java/com/ingrid/simuladordepartidas/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.ingrid.simuladordepartidas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ingrid.simuladordepartidas.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
